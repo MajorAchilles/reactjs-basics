cc574547b26a96cfe960e6381271bd95d794ff49
MajorAchilles <amlanjyoti.s@gmail.com>
2019-07-04 00:01:39 +0530

    Hello world

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("It's alive!!!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!!!! I am alive!!!!!</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>, document.querySelector("#root"));
