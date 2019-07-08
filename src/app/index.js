83ece807e06f8ca85aa4ab54e568263b702fb584
Saikia, Amlan <amlan.saikia@cerner.com>
2019-07-08 09:29:11 +0530

    REDUX

--- src/app/index.js
+++ src/app/index.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { render } from "react-dom";
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import {render} from "react-dom";
+
+import { User } from './components/User';
+import { Main } from './components/Main';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "Max"
+        };
+    }
+
+    changeUsername(newName) {
+        this.setState({
+            username: newName
+        });
+    }
+
     render() {
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
-                    </div>    
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} age={27}/>
-                    </div>    
-                </div>
+                <Main changeUsername={this.changeUsername.bind(this)}/>
+                <User username={this.state.username}/>
             </div>
         );
     }
 }
 
-render(<App/>, document.querySelector("#root"));
+render(<App />, window.document.getElementById('app'));
